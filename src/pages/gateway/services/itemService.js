ad37053c2d5377ca472cf45cae86b8273ac4cd25
fernando <sousa.fernandocastro@gmail.com>
2023-09-25 12:02:16 -0300

    :sparkles: feat: build

--- src/pages/gateway/services/itemService.js
+++ src/pages/gateway/services/itemService.js
@@ -1,8 +1,6 @@
 import { api } from "@/services/api";
 
-const itemService = {
+export const itemService = {
   get: (params) => (params ? api.get(`/item/${params}`) : api.get(`/items`)),
   post: (data) => api.post(`/item`, data),
 };
-
-export default itemService;
