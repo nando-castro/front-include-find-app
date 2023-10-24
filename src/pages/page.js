e113958aa219d4c805a199f131f580fd3129543d
nando-castro <fernando.castro10.master@gmail.com>
2023-10-24 11:15:11 -0300

    :sparkles: feat: atualização de código

--- src/pages/page.js
+++ src/pages/page.js
@@ -4,16 +4,14 @@ import { Rubik_Iso } from "next/font/google";
 import Head from "next/head";
 import { useState } from "react";
 
-
 const rubik = Rubik_Iso({ weight: "400", subsets: ["latin"] });
 
-  const apiUrl = "https://back-include-find-api.onrender.com";
-  // const apiUrl = process.env.API_URL;
+const apiUrl = "https://back-include-find-api.onrender.com";
+// const apiUrl = process.env.API_URL;
 
 export default function Home() {
   const [name, setName] = useState("");
   const [users, setUsers] = useState([]);
-
 
   function changeInput(e) {
     setName(e.target.value);
@@ -21,18 +19,19 @@ export default function Home() {
 
   function include() {
     if (name === "") return alert("Digite um nome!");
-   axios
-      .post(`${apiUrl}/user`,{ name: name })
+    axios
+      .post(`${apiUrl}/user`, { name: name })
       .then((response) => {
         console.log(response.data);
+        alert("Usuário cadastrado com sucesso!");
       })
       .catch((error) => {
         console.log(error);
         if (error.response.status === 422) {
           alert("Nome já cadastrado!");
         }
-        if(error.response.status === 500){
-          alert("Erro no servidor!")
+        if (error.response.status === 500) {
+          alert("Erro no servidor!");
         }
       });
   }
@@ -40,13 +39,13 @@ export default function Home() {
   function find() {
     if (name === "") return alert("Digite um nome!");
     axios
-      .get(`${apiUrl}/user`,name)
+      .get(`${apiUrl}/user`, name)
       .then((response) => {
         setUsers(response.data);
       })
       .catch((error) => {
         console.log(error);
-        alert("Usuário não encontrado!")
+        alert("Usuário não encontrado!");
       });
   }
 
@@ -70,46 +69,50 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-      <div className={styles.top}>
-        <p className={rubik.className}>Include-Find App</p>
-      </div>
-      <div className={styles.middle}>
-        <div className={styles.containerOptions}>
-          <form className={styles.form}>
-            <input
-              maxLength={20}
-              className={styles.input}
-              type="text"
-              placeholder="Nome"
-              name="name"
-              onChange={changeInput}
-            />
-          </form>
-          <div className={styles.containerButtons}>
-            <div className={styles.optionInclude} onClick={include}>
-              Cadastrar
+        <div className={styles.top}>
+          <p className={rubik.className}>Include-Find App</p>
+        </div>
+        <div className={styles.middle}>
+          <div className={styles.containerOptions}>
+            <form className={styles.form}>
+              <input
+                maxLength={20}
+                className={styles.input}
+                type="text"
+                placeholder="Nome"
+                name="name"
+                onChange={changeInput}
+              />
+            </form>
+            <div className={styles.containerButtons}>
+              <div className={styles.optionInclude} onClick={include}>
+                Cadastrar
+              </div>
+              <div className={styles.optionFindAll} onClick={findAll}>
+                Buscar Todos
+              </div>
+              <div className={styles.optionFind} onClick={find}>
+                Buscar
+              </div>
             </div>
-            <div className={styles.optionFindAll} onClick={findAll}>
-              Buscar Todos
-            </div>
-            <div className={styles.optionFind} onClick={find}>
-              Buscar
-            </div>
+          </div>
+          <div className={styles.containerItems}>
+            <p className={styles.list}>Resultado:</p>
+            {users.length > 0 ? (
+              users.map((item, index) => (
+                <div className={styles.item} key={index}>
+                  {item.name}
+                </div>
+              ))
+            ) : (
+              <div>Não há usuários cadastrados!</div>
+            )}
           </div>
         </div>
-        <div className={styles.containerItems}>
-            <p className={styles.list}>Resultado:</p>
-            {users.length > 0 ? users.map((item, index) => (
-              <div className={styles.item} key={index}>
-                {item.name}
-              </div>
-            )) : <div>Não há usuários cadastrados!</div>}
-          </div>
-      </div>
-      <div className={styles.bottom}>
-        <p className={rubik.className}>2023 &copy;</p>
-      </div>
-    </main>
+        <div className={styles.bottom}>
+          <p className={rubik.className}>2023 &copy;</p>
+        </div>
+      </main>
     </>
   );
 }
